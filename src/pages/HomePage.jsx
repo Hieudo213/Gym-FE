bf5874de9db6c5da874bb17007c7aefc53b93df1
Hieudo213 <hieudoo213@gmail.com>
2024-10-08 21:52:50 +0700

    saved changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,15 +2,7 @@ import React from "react";
 
 // react hook
 function HomePage() {
-<<<<<<< HEAD
   return <div>HomePage</div>;
-=======
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
->>>>>>> 303d971ab617296c82e5b7c4d1a883fe7e77e6b1
 }
 
 export default HomePage;
