ea22ab115f10610d0f4ae01ae184ab05c1b3a4ed
VuDat <129578535+datmp18@users.noreply.github.com>
2024-10-18 09:56:34 +0700

    Feature/datvu (#21)
    * gym-8
    
    * update
    
    * updated
    
    * GYM-17
    
    * GYM-GYM17 MAIN
    
    ---------
    
    Co-authored-by: DoHieu <102859516+Hieudo213@users.noreply.github.com>

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -10,50 +10,42 @@ function Footer() {
   return (
     <>
       <div className="footer_body">
-        <div className="list">
-          <div className="col-lg-4">
-            <div className="text_logo">GYMPRO</div>
-            <div className="item1">
-              <i className="fa-solid fa-phone fix1"></i>
-              <span>037 573 4367</span>
+        <div class="w-[85%] mx-auto flex">
+          <div class="w-[30%]   h-full">
+            <div className="text_logo font-bold text-base">GYMPRO</div>
+            <div className="row mt-5">
+              <i class="fa-solid fa-phone mt-1"></i>
+              <span className="text-base ml-3">1900 633 638</span>
             </div>
-            <div className="item1">
-
-              <i className="fa-solid fa-location-dot fix1"></i>
-              <span>
-                Sân vận động Bách khoa , 94.Lê Thanh Nghị, Q.Hai Bà Trưng,Tp.Hà
-                Nội
-              </span>
+            <div className="row mt-5">
+              <i class="fa-solid fa-location-dot"></i>
+              <span className="text-base ml-3">92,Lê Thanh Nghị , Hai bà Trưng , Hà Nội (Sân vân động Bách Khoa)</span>
             </div>
-            <div className="item1">
-              <span className="icon_block">
-                <i className="fa-solid fa-clock fix1 "></i>
-              </span>
-
-              <span className="text2">
+            <div className="row mt-5 flex">
+              <div className="w-[5%] flex items-center justify-center mr-2">
+                <i class="fa-solid fa-clock"></i>
+              </div>
+              <div className="w-[90%]">
 
                 <p>Thứ Hai - Thứ Sáu: 6:00 đến 23:00 </p>
-                <p>Thứ Bảy - Chủ Nhật: 8:00 đến 22:00</p>
-              </span>
+                <p>Thứ Bảy - Chủ nhật: 8:00 đến 22:00 </p>
+              </div>
             </div>
           </div>
-
-          <div className="col-lg-4">
-            <div className="col-lg-6">
-              <div className="item1">Phương thức thanh toán </div>
-              <div className="item1">Dịch Vụ</div>
-              <div className="item1">Chính sách giá</div>
-              <div className="item1">Chính sách bảo mật</div>
-            </div>
-            <div className="col-lg-6">
-              <div className="item1">Tuyển dụng </div>
-              <div className="item1">Tin tức</div>
-              <div className="item1">Liên hệ </div>
-              <div className="item1">Câu lạc bộ</div>
-            </div>
+          <div class="w-[20%]  mt-5 p-3 mx-auto pt-2 h-full">
+            <div className="row  mt-5 justify-center">Phương thức thanh toán</div>
+            <div className="row  mt-5 justify-center">Dịch vụ</div>
+            <div className="row  mt-5 justify-center">Chính sách giá </div>
+            <div className="row  mt-5 justify-center">Chính sách bảo mật</div>
           </div>
 
-          <div className="col-lg-4">
+          <div class="w-[20%]  mt-5 mx-auto pt-2  h-full">
+            <div className="row  mt-5 justify-center"> Tuyển dụng </div>
+            <div className="row  mt-5 justify-center">Tin tức </div>
+            <div className="row  mt-5 justify-center">Liên hệ</div>
+            <div className="row  mt-5 justify-center">Câu lạc bộ</div>
+          </div>
+          <div class="w-[30%]   h-full">
 
             <div className="text_logo fix6">Thành viên </div>
 
@@ -113,6 +105,58 @@ function Footer() {
         </div>
 
       </div>
+      {/* <div className="col-lg-4">
+
+        <div className="text_logo fix6">Thành viên </div>
+
+        <div className="Designed">
+          <Link
+            to="https://www.facebook.com/profile.php?id=100082315533736"
+            className="item_desig"
+          >
+            <div className="image_item">
+              <img src={Dat} alt="" />
+            </div>
+          </Link>
+
+          <Link
+            to="https://www.facebook.com/lam.th1042"
+            className="item_desig"
+          >
+            <div className="image_item">
+              <img src={Lam} alt="" />
+            </div>
+          </Link>
+
+          <Link
+            to="https://www.facebook.com/hieudoo213"
+            className="item_desig"
+          >
+            <div className="image_item">
+              <img src={Hieu} alt="" />
+            </div>
+          </Link>
+        </div>
+
+        <div className="Designed">
+          <Link
+            to="https://www.facebook.com/pham.congdung.351"
+            className="item_desig "
+          >
+            <div className="image_item">
+              <img src={Dung} alt="" />
+            </div>
+          </Link>
+          <Link
+            to="https://www.facebook.com/profile.php?id=100014609829736"
+            className="item_desig"
+          >
+            <div className="image_item">
+              <img src={Tanh} className="fixa" alt="" />
+            </div>
+          </Link>
+        </div>
+      </div> */}
     </>
   );
 }
