04ef2c243f58f434744f1f1ea225d49c73dff729
vudat <vutiendat1804hd@gmail.com>
2024-11-08 22:24:21 +0700

    test

--- src/components/calendar.jsx
+++ src/components/calendar.jsx
@@ -1,100 +1,19 @@
 import React from 'react';
-import { Badge, Calendar } from 'antd';
-const getListData = (value) => {
-    let listData = []; // Specify the type of listData
-    switch (value.date()) {
-        case 8:
-            listData = [
-                {
-                    type: 'warning',
-                    content: 'This is warning event.',
-                },
-                {
-                    type: 'success',
-                    content: 'This is usual event.',
-                },
-            ];
-            break;
-        case 10:
-            listData = [
-                {
-                    type: 'warning',
-                    content: 'This is warning event.',
-                },
-                {
-                    type: 'success',
-                    content: 'This is usual event.',
-                },
-                {
-                    type: 'error',
-                    content: 'This is error event.',
-                },
-            ];
-            break;
-        case 15:
-            listData = [
-                {
-                    type: 'warning',
-                    content: 'This is warning event',
-                },
-                {
-                    type: 'success',
-                    content: 'This is very long usual event......',
-                },
-                {
-                    type: 'error',
-                    content: 'This is error event 1.',
-                },
-                {
-                    type: 'error',
-                    content: 'This is error event 2.',
-                },
-                {
-                    type: 'error',
-                    content: 'This is error event 3.',
-                },
-                {
-                    type: 'error',
-                    content: 'This is error event 4.',
-                },
-            ];
-            break;
-        default:
-    }
-    return listData || [];
+import { Calendar, theme } from 'antd';
+const onPanelChange = (value, mode) => {
+    console.log(value.format('YYYY-MM-DD'), mode);
 };
-const getMonthData = (value) => {
-    if (value.month() === 8) {
-        return 1394;
-    }
+const CalendarComponent = () => {
+    const { token } = theme.useToken();
+    const wrapperStyle = {
+        width: 340,
+        border: `1px solid ${token.colorBorderSecondary}`,
+        borderRadius: token.borderRadiusLG,
+    };
+    return (
+        <div style={wrapperStyle}>
+            <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+        </div>
+    );
 };
-const App = () => {
-    const monthCellRender = (value) => {
-        const num = getMonthData(value);
-        return num ? (
-            <div className="notes-month">
-                <section>{num}</section>
-                <span>Backlog number</span>
-            </div>
-        ) : null;
-    };
-    const dateCellRender = (value) => {
-        const listData = getListData(value);
-        return (
-            <ul className="events">
-                {listData.map((item) => (
-                    <li key={item.content}>
-                        <Badge status={item.type} text={item.content} />
-                    </li>
-                ))}
-            </ul>
-        );
-    };
-    const cellRender = (current, info) => {
-        if (info.type === 'date') return dateCellRender(current);
-        if (info.type === 'month') return monthCellRender(current);
-        return info.originNode;
-    };
-    return <Calendar cellRender={cellRender} />;
-};
-export default App;
+export default CalendarComponent;
--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,12 +1,115 @@
 import React from "react";
+import { CiCamera } from "react-icons/ci";
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineWorkHistory } from "react-icons/md";
+import CalendarComponent from "../components/calendar";
+import { Table } from "antd";
 
+const dataSource = [
+  {
+    key: '1',
+    id: '1',
+    money: 32,
+    time: '02/10/2024 12:08',
+    content: "Mua gói Platinum",
+    accountNumber: "01234567890",
+  },
+  {
+    key: '2',
+    id: '2',
+    money: 42,
+    time: '02/10/2024 12:08',
+    content: "Mua gói Platinum",
+    accountNumber: "01234567890",
+  },
+  {
+    key: '3',
+    id: '3',
+    money: 32,
+    time: '02/10/2024 12:08',
+    content: "Mua gói Platinum",
+    accountNumber: "01234567890",
+  },
+  {
+    key: '4',
+    id: '4',
+    money: 42,
+    time: '02/10/2024 12:08',
+    content: "Mua gói Platinum",
+    accountNumber: "01234567890",
+  },
+  {
+    key: '5',
+    id: '5',
+    money: 32,
+    time: '02/10/2024 12:08',
+    content: "Mua gói Platinum",
+    accountNumber: "01234567890",
+  },
+  {
+    key: '6',
+    id: '6',
+    money: 42,
+    time: '02/10/2024 12:08',
+    content: "Mua gói Platinum",
+    accountNumber: "01234567890",
+  },
+  {
+    key: '7',
+    id: '7',
+    money: 32,
+    time: '02/10/2024 12:08',
+    content: "Mua gói Platinum",
+    accountNumber: "01234567890",
+  },
+  {
+    key: '8',
+    id: '8',
+    money: 42,
+    time: '02/10/2024 12:08',
+    content: "Mua gói Platinum",
+    accountNumber: "01234567890",
+  },
+];
+
+const columns = [
+  {
+    title: 'Mã Giao Dịch',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: 'Số Tiền',
+    dataIndex: 'money',
+    key: 'money',
+  },
+  {
+    title: 'Thời gian giao dịch',
+    dataIndex: 'time',
+    key: 'time',
+  },
+  {
+    title: 'Nội dung giao dịch',
+    dataIndex: 'content',
+    key: 'content',
+  },
+  {
+    title: 'Số tài khoản',
+    dataIndex: 'accountNumber',
+    key: 'accountNumber',
+  },
+];
 const Schedule = () => {
   return (
-    <div className="mt-[5rem] w-full ">
-      <div className="w-4/5 mx-auto grid grid-cols-2 gap-1 mt-10">
-        <div className="group">attt</div>
-        <div className="group">átdd</div>
 
+    <div className="max-w-[1180px] mt-[7rem]  mx-auto h-[25rem]  my-[3rem]    flex p-2">
+      <div className=" w-1/3 max-w-[40%]   border border-gray-400 p-4  rounded-[0.5rem]">
+        <CalendarComponent />
+      </div>
+      <div className=" w-2/3   px-1 border border-gray-400 py-4  rounded-[0.5rem] fix10 ml-4">
+        <div className='w-full h-full overflow-scroll overflow-x-scroll'>
+          <Table className='w-fit' dataSource={dataSource} columns={columns} pagination={false} />
+        </div>
       </div>
     </div>
   )
