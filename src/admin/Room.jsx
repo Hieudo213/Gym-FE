0933b834a1658fed51927bae17044ba36407366c
Hieudo213 <hieudoo213@gmail.com>
2024-11-29 22:41:49 +0700

    updated

--- src/admin/Room.jsx
+++ src/admin/Room.jsx
@@ -1,4 +1,4 @@
-import { Checkbox } from 'antd';
+import { Checkbox, Form, Input, Modal, Radio, Button } from 'antd';
 import React, { useState } from 'react';
 import Table_list from './Model/Table_list';
 import Toolbar from './Model/Toolbar';
@@ -7,6 +7,11 @@ export const Room = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [loading, setLoading] = React.useState(true);
     const [open, setOpen] = React.useState(false);
+    const [form] = Form.useForm();
+    const [formLayout, setFormLayout] = useState('horizontal');
+    const onFormLayoutChange = ({ layout }) => {
+        setFormLayout(layout);
+    };
     const dataSource = [
         {
             key: '1',
@@ -84,7 +89,46 @@ export const Room = () => {
             <div className='w-full flex p-3'  >
                 <Table_list columns={columns} dataSource={dataSource} />
             </div>
-
+            <Modal title="them moi" open={false} >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
+            <Modal title="Cập nhật Thể loại" open={true} >
+                <Form
+                    layout={formLayout}
+                    form={form}
+                    initialValues={{
+                        layout: formLayout,
+                    }}
+                    onValuesChange={onFormLayoutChange}
+                    style={{
+                        maxWidth: formLayout === 'inline' ? 'none' : 600,
+                    }}
+                >
+                    <Form.Item label="Form Layout" name="layout">
+                        <Radio.Group value={formLayout}>
+                            <Radio.Button value="horizontal">Horizontal</Radio.Button>
+                            <Radio.Button value="vertical">Vertical</Radio.Button>
+                            <Radio.Button value="inline">Inline</Radio.Button>
+                        </Radio.Group>
+                    </Form.Item>
+                    <Form.Item label="Field A">
+                        <Input placeholder="input placeholder" />
+                    </Form.Item>
+                    <Form.Item label="Field B">
+                        <Input placeholder="input placeholder" />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary">Submit</Button>
+                    </Form.Item>
+                </Form>
+            </Modal>
+            <Modal title="Cập các môn của thể loại" open={false} >
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+                <p>Some contents...</p>
+            </Modal>
         </>
     );
 };
