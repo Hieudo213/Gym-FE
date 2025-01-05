8204c2f3d341d0821c8e90b375da703aa7ef277b
vudat <vutiendat1804hd@gmail.com>
2025-01-05 14:43:11 +0700

    update lịch tập

--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -9,93 +9,9 @@ import { faCircleArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 function Login() {
   return (
-    <main className="main-container">
-      <div className="login-container">
-        <section className="login-section">
-          <div className="form-container">
-            <div className="form-wrapper">
-              <div className="form-card">
-                <div className="form-card-body ">
-                  <div className="form-header">
-                    <div className="form-title   ">
-                      <span className="icon float-left mt-1  w-5 h-5 text-xl hover:text-green-300  transition duration-200 cursor-pointer" >
-                        <Link to="/">
-                          <FontAwesomeIcon icon={faCircleArrowLeft} />
-                        </Link>
-                      </span>
-                      <span>  Đăng nhập</span>
-                    </div>
-                  </div>
-                  <form className="login-form" noValidate>
-                    <div className="form-group">
-                      <label htmlFor="yourUsername" className="form-label">
-                        Tên người dùng
-                      </label>
-                      <div className="input-wrapper">
-                        <input
-                          type="text"
-                          name="username"
-                          className="form-input"
-                          id="yourUsername"
-                          required
-                        />
-                        <div className="error-message">
-                          Vui lòng nhập tên người dùng của bạn.
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <label htmlFor="yourPassword" className="form-label">
-                        Mật khẩu
-                      </label>
-                      <input
-                        type="password"
-                        name="password"
-                        className="form-input"
-                        id="yourPassword"
-                        required
-                      />
-                      <div className="error-message">
-                        Vui lòng nhập mật khẩu của bạn!
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <div className="checkbox-wrapper">
-                        <input
-                          className="checkbox-input"
-                          type="checkbox"
-                          name="remember"
-                          value="true"
-                          id="rememberMe"
-                        />
-                        <label className="checkbox-label" htmlFor="rememberMe">
-                          Ghi nhớ đăng nhập
-                        </label>
-                      </div>
-                    </div>
-
-                    <div className="form-group">
-                      <button className="submit-button" type="submit">
-                        Đăng nhập
-                      </button>
-                    </div>
-
-                    <div className="form-footer">
-                      <p className="form-text">
-                        Chưa có tài khoản?{" "}
-                        <Link to="/register">Tạo tài khoản</Link>
-                      </p>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
-    </main>
+    <div className="w-full h-[100vh] flex items-center justify-center">
+      <div className="w-3/4 p-5 bg-black"></div>
+    </div>
   );
 }
 
--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -1,114 +1,90 @@
 import React from "react";
-
-
 import CalendarComponent from "../components/calendar";
-import { Table } from "antd";
 
-const dataSource = [
+import { Image, Collapse }
+  from
+  "antd"
+  ;
+import { IoWarningOutline } from "react-icons/io5";
+import { FaClock } from "react-icons/fa";
+
+const title = (rank, teacher, time, date, status = 0) => (
+  <div className="w-full h-full relative">
+    <p className="flex text-black">
+      {status !== 0 ? (
+        <span className="mr-2 mt-[2px] text-yellow-300">
+          <IoWarningOutline size={17} />
+        </span>
+      ) : null}
+      Bạn có một buổi học cùng {rank} {teacher}
+    </p>
+    <div className="absolute top-0 right-0 w-[200px] h-full flex text-red-500">
+      <span className="mr-1 mt-[4px]">
+        <FaClock />
+      </span>
+      <span>
+        {time} | {date}
+      </span>
+    </div>
+  </div>
+);
+const content = (room, gym, address) => (
+  <div>
+    <h2>
+      Phòng: <span className="text-red-500">{room}</span>
+    </h2>
+    <h2>
+      Tại club: <span className="text-red-500">{gym}</span>
+    </h2>
+    <h2>
+      Địa chỉ: <span className="text-red-500">{address}</span>
+    </h2>
+    <p className="text-blue-500">Chúc bạn 1 ngày vui vẻ cùng Gympro!</p>
+  </div>
+);
+
+const items = [
   {
     key: '1',
-    id: '1',
-    money: 32,
-    time: '02/10/2024 12:08',
-    content: "Mua gói Platinum",
-    accountNumber: "01234567890",
+    label: title('Giảng viên', 'Đạt', '7:00', '06/01/2025', 1),
+    children: content(205, 'Yoga', '99, p. Lê Thanh Nghị, Bách khoa, Hai Bà Trưng, Hà Nội'),
   },
   {
     key: '2',
-    id: '2',
-    money: 42,
-    time: '02/10/2024 12:08',
-    content: "Mua gói Platinum",
-    accountNumber: "01234567890",
+    label: title('Trợ giảng', 'Thanh Lâm', '14:00', '08/01/2025', 0),
+    children: content(203, 'Gym', '99, p. Lê Thanh Nghị, Bách khoa, Hai Bà Trưng, Hà Nội'),
   },
   {
     key: '3',
-    id: '3',
-    money: 32,
-    time: '02/10/2024 12:08',
-    content: "Mua gói Platinum",
-    accountNumber: "01234567890",
-  },
-  {
-    key: '4',
-    id: '4',
-    money: 42,
-    time: '02/10/2024 12:08',
-    content: "Mua gói Platinum",
-    accountNumber: "01234567890",
-  },
-  {
-    key: '5',
-    id: '5',
-    money: 32,
-    time: '02/10/2024 12:08',
-    content: "Mua gói Platinum",
-    accountNumber: "01234567890",
-  },
-  {
-    key: '6',
-    id: '6',
-    money: 42,
-    time: '02/10/2024 12:08',
-    content: "Mua gói Platinum",
-    accountNumber: "01234567890",
-  },
-  {
-    key: '7',
-    id: '7',
-    money: 32,
-    time: '02/10/2024 12:08',
-    content: "Mua gói Platinum",
-    accountNumber: "01234567890",
-  },
-  {
-    key: '8',
-    id: '8',
-    money: 42,
-    time: '02/10/2024 12:08',
-    content: "Mua gói Platinum",
-    accountNumber: "01234567890",
-  },
-];
-
-const columns = [
-  {
-    title: 'Mã Giao Dịch',
-    dataIndex: 'id',
-    key: 'id',
-  },
-  {
-    title: 'Số Tiền',
-    dataIndex: 'money',
-    key: 'money',
-  },
-  {
-    title: 'Thời gian giao dịch',
-    dataIndex: 'time',
-    key: 'time',
-  },
-  {
-    title: 'Nội dung giao dịch',
-    dataIndex: 'content',
-    key: 'content',
-  },
-  {
-    title: 'Số tài khoản',
-    dataIndex: 'accountNumber',
-    key: 'accountNumber',
+    label: title('Giảng viên', 'Đỗ Ngọc Hiếu', '20:00', '18/01/2025', 0),
+    children: content(201, 'Boxing', '99, p. Lê Thanh Nghị, Bách khoa, Hai Bà Trưng, Hà Nội'),
   },
 ];
 const Schedule = () => {
+  const onChange = (key) => {
+    console.log(key);
+  };
   return (
-
-    <div className="max-w-[1180px] my-[20vh]  mx-auto h-[25rem]    flex p-2">
+    <div className="max-w-[1180px] mt-[20vh] my-[10vh]  mx-auto h-[70vh]  flex p-2">
       <div className=" w-1/3 max-w-[40%]   border border-gray-400 p-4  rounded-[0.5rem]">
         <CalendarComponent />
       </div>
-      <div className=" w-2/3   px-1 border border-gray-400 py-4  rounded-[0.5rem] fix10 ml-4">
-        <div className='w-full h-full overflow-scroll overflow-x-scroll'>
-          <Table className='w-fit' dataSource={dataSource} columns={columns} pagination={false} />
+      <div className=" w-2/3   p-1 border border-gray-400   rounded-[0.5rem] fix10 ml-4">
+        <div className='w-full  flex'>
+          <div className=" w-1/6 aspect-square  ">
+            <Image src="https://cdn-i.vtcnews.vn/files/huongntt/2019/08/24/giaovien2-7-0650272.jpg" className="rounded-[50%] p-6 " alt="" />
+          </div>
+          <div className="w-4/5 pt-6 ">
+            <h1 className="text-[2rem] text-bold">Vũ Tiến Đạt</h1>
+            <h4>Vai trò : <span>Giảng viên Yoga</span></h4>
+          </div>
+
         </div>
+        <div className="w-full h-[40vh] mt-[-20px] overflow-y-scroll ">
+          <Collapse items={items} defaultActiveKey={['1']} onChange={onChange} />
+        </div>
+
+
       </div>
     </div>
   )
