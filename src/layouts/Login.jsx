446cbf1cbaa05db5aa4f4a0222b4c157b37116ff
vudat <vutiendat1804hd@gmail.com>
2025-01-06 02:42:48 +0700

    update login

--- src/layouts/Login.jsx
+++ src/layouts/Login.jsx
@@ -6,6 +6,7 @@ import logo from "../assets/img/logo/favicon.png"
 
 import { MdEmail } from "react-icons/md";
 import { FaLock } from "react-icons/fa";
+import { BsArrowLeft } from "react-icons/bs";
 
 function Login() {
   const onFinish = (values) => {
@@ -17,60 +18,63 @@ function Login() {
         <img src={logo} className="w-full h-full  " alt="" />
       </div>
       <div className="  w-4/5 [clip-path:polygon(0%_100%,0%_0%,60%_0%,100%_100%)]  h-[100vh] bg-red-600 "></div>
-      <div className=" w-2/3  bg-gray-200  rounded-[10px]   flex z-9 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" style={{ boxShadow: '1px 2px 5px black' }}>
-        <div className="w-1/2    rounded-tl-lg rounded-bl-lg "><img src="https://img.tripi.vn/cdn-cgi/image/width=700,height=700/https://gcs.tripi.vn/public-tripi/tripi-feed/img/474229OmO/hinh-nen-gym-buon-dep_032118243.jpg" className="rounded-tl-lg rounded-bl-lg w-full h-full" alt="" /> </div>
-        <div className="w-1/2 h-full p-2">
-          <h1 className="font-bold text-2xl  text-center mb-[2rem]">Đăng nhập</h1>
-          <Form
-            name="login"
-            initialValues={{
-              remember: true,
-            }}
-            style={{
-              maxWidth: 360,
-              margin: 'auto'
-            }}
-            onFinish={onFinish}
+      <div className=" reletive w-1/3 p-3  bg-white  rounded-[10px]   z-9 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" style={{ boxShadow: '1px 5px 10px black' }}>
+        <Link to="/">
+          <div className="absolute text-lg top-2 left-6 rounded-[50%] w-[2.2rem] h-[2.2rem]  border border-gray-200 flex justify-center items-center " style={{ boxShadow: '1px 2px 3px while' }}>
+            <BsArrowLeft />
+          </div>
+        </Link>
+        <h1 className="font-bold text-2xl  text-center mb-[2rem]">Đăng nhập</h1>
+        <Form
+          name="login"
+          initialValues={{
+            remember: true,
+          }}
+          style={{
+            maxWidth: 360,
+            margin: 'auto'
+          }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
           >
-            <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Username!',
-                },
-              ]}
-            >
-              <Input prefix={<MdEmail />} placeholder="Email" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Password!',
-                },
-              ]}
-            >
-              <Input prefix={<FaLock />} type="password" placeholder="Password" />
-            </Form.Item>
-            <Form.Item>
-              <Flex justify="space-between" align="center">
-                <Form.Item name="remember" valuePropName="checked" noStyle>
-                  <Checkbox>Remember me</Checkbox>
-                </Form.Item>
-                <a href="">Forgot password</a>
-              </Flex>
-            </Form.Item>
+            <Input prefix={<MdEmail />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input prefix={<FaLock />} type="password" placeholder="Mật khẩu" />
+          </Form.Item>
+          <Form.Item>
+            <Flex justify="space-between" align="center">
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Lưu thông tin của tôi</Checkbox>
+              </Form.Item>
+              <a href="">Quên mật khẩu ?</a>
+            </Flex>
+          </Form.Item>
 
-            <Form.Item>
-              <Button block type="primary" htmlType="submit">
-                Log in
-              </Button>
-              or <Link to="/register"> Register now!</Link>
-            </Form.Item>
-          </Form>
-        </div>
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">
+              Log in
+            </Button>
+            Về trang <Link to="/register"> Đăng ký ngay !</Link>
+          </Form.Item>
+        </Form>
+
       </div>
     </div>
   );
--- src/layouts/Register.jsx
+++ src/layouts/Register.jsx
@@ -6,6 +6,7 @@ import logo from "../assets/img/logo/favicon.png"
 
 import { MdEmail } from "react-icons/md";
 import { FaLock } from "react-icons/fa";
+import { BsArrowLeft } from "react-icons/bs";
 
 
 
@@ -19,67 +20,69 @@ function Register() {
         <img src={logo} className="w-full h-full  " alt="" />
       </div>
       <div className="float-right  w-4/5 [clip-path:polygon(60%_100%,0%_0%,100%_0%,100%_100%)]  z-0  h-[100vh] bg-red-600 "></div>
-      <div className=" w-2/3  bg-gray-200  rounded-[10px]   flex z-9 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" style={{ boxShadow: '1px 2px 5px black' }}>
-        <div className="w-1/2    rounded-tl-lg rounded-bl-lg ">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9zWkpSNAlnszt6MVHID3A2tfe91X8HPP3qA&s" className="rounded-tl-lg rounded-bl-lg w-full h-full" alt="" /> </div>
-        <div className="w-1/2 h-full p-2">
-          <h1 className="font-bold text-2xl  text-center mb-[2rem]">Đăng Ký</h1>
-          <Form
-            name="login"
-            initialValues={{
-              remember: true,
-            }}
-            style={{
-              maxWidth: 360,
-              margin: 'auto'
-            }}
-            onFinish={onFinish}
+
+      <div className="reletive p-3 w-1/3  bg-white rounded-[10px]   z-9 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" style={{ boxShadow: '1px 5px 10px black' }}>
+        <Link to="/">
+          <div className="absolute text-lg top-2 left-6 rounded-[50%] w-[2.2rem] h-[2.2rem]  border border-gray-200 flex justify-center items-center " style={{ boxShadow: '1px 2px 3px while' }}>
+            <BsArrowLeft />
+          </div>
+        </Link>
+        <h1 className="font-bold text-2xl  text-center mb-[2rem]">Đăng Ký</h1>
+        <Form
+          name="login"
+          initialValues={{
+            remember: true,
+          }}
+          style={{
+            maxWidth: 360,
+            margin: 'auto'
+          }}
+          onFinish={onFinish}
+        >
+
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
           >
-
-            <Form.Item
-              name="username"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Username!',
-                },
-              ]}
-            >
-              <Input prefix={<MdEmail />} placeholder="Email" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Password!',
-                },
-              ]}
-            >
-              <Input prefix={<FaLock />} type="password" placeholder="Password" />
-            </Form.Item>
-            <Form.Item
-              name="confirmpassword "
-              rules={[
-                {
-                  required: true,
-                  message: 'Please input your Password!',
-                },
-              ]}
-            >
-              <Input prefix={<FaLock />} type="password" placeholder="Confirm password" />
-            </Form.Item>
+            <Input prefix={<MdEmail />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input prefix={<FaLock />} type="password" placeholder="Mật khẩu" />
+          </Form.Item>
+          <Form.Item
+            name="confirmpassword "
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input prefix={<FaLock />} type="password" placeholder="Nhập lại mật khẩu " />
+          </Form.Item>
 
 
-            <Form.Item>
-              <Button block type="primary" htmlType="submit">
-                Register
-              </Button>
-              or  <Link to="/login">Login now! </Link>
+          <Form.Item>
+            <Button block type="primary" htmlType="submit">
+              Register
+            </Button>
+            Về trang  <Link to="/login">Đăng nhập ngay! </Link>
 
-            </Form.Item>
-          </Form>
-        </div>
+          </Form.Item>
+        </Form>
       </div>
     </div>
   );
