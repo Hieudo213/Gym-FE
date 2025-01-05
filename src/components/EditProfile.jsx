71008ea1bd7f2f5e10511b435297ff9cb4561ed4
vudat <vutiendat1804hd@gmail.com>
2025-01-06 03:30:53 +0700

    update form edit account

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,52 +1,101 @@
 import React from 'react'
-
+import { Button, DatePicker, Form, Input, InputNumber, Select, Space, Tooltip, Typography } from 'antd';
+const { Option } = Select;
+const onFinish = (values) => {
+    console.log('Received values of form: ', values);
+};
 function EditProfile() {
     return (
-        <form action="">
+        <>
             <h1 className="text-xl mb-2 font-bold text-[#b30c0d]">Thông tin cá nhân</h1>
-            <hr />
-            <div className="grid gap-6 mb-6 md:grid-cols-2 mt-5">
-                <div>
-                    <label for="first_name" className="block  text-sm font-medium text-gray-900 ">Họ và Tên</label>
-                    <input type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " defaultValue="Vũ Tiến Đạt" />
-                </div>
+            <hr className='mb-5' />
+            <Form
+                name="complex-form"
+                onFinish={onFinish}
 
-                <div>
-                    <label for="last_name" className="block  text-sm font-medium text-gray-900 ">Ngày sinh</label>
-                    <input type="date" id="last_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " />
-                </div>
+                style={{
+                    maxWidth: '100%',
+                }}
+            >
+                <Form.Item >
+                    <Input placeholder='Usename' />
+                </Form.Item>
+                <Form.Item
 
-                <div>
-                    <label for="company" className="block  text-sm font-medium text-gray-900 ">Email</label>
-                    <input type="email" id="company" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " defaultValue="vutiendat1804hd@gmail.com" />
-                </div>
+                    style={{
+                        marginBottom: 0,
+                    }}
+                >
+                    <Form.Item
+                        name="firstname"
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                        style={{
+                            display: 'inline-block',
+                            width: 'calc(50% - 8px)',
+                        }}
+                    >
+                        <Input placeholder="Họ " />
+                    </Form.Item>
+                    <Form.Item
+                        name="lastname"
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                        style={{
+                            display: 'inline-block',
+                            width: 'calc(50% - 8px)',
+                            margin: '0 8px',
+                        }}
+                    >
+                        <Input placeholder="Tên" />
+                    </Form.Item>
+                </Form.Item>
+                <Form.Item
 
-                <div>
-                    <label for="countries" className="block text-sm font-medium text-gray-900 ">Giới tính</label>
-                    <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ">
+                    style={{
+                        marginBottom: 0,
+                    }}
+                >
+                    <Form.Item
+                        name="birthday"
 
-                        <option>Khác</option>
-                        <option>Nam</option>
-                        <option>Nữ</option>
+                        style={{
+                            display: 'inline-block',
+                            width: 'calc(50% - 8px)',
+                        }}
+                    >
+                        <DatePicker placeholder='Ngày sinh' />
+                    </Form.Item>
+                    <Form.Item
+                        name="age"
 
-                    </select>
-                </div>
-                <div>
-                    <label for="website" className="block text-sm font-medium text-gray-900 ">Số điện thoại</label>
-                    <input type="text" id="website" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" defaultValue="32094722" />
-                </div>
-                <div>
-                    <label for="visitors" className="block  text-sm font-medium text-gray-900 ">Nghề nghiệp</label>
-                    <input type="text" id="visitors" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Khác" />
-                </div>
-            </div>
-            <div className="mb-6">
-                <label for="email" className="block  text-sm font-medium text-gray-900 dark:text-white">Địa chỉ</label>
-                <input type="text" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="Hà Nội" />
-            </div>
+                        style={{
+                            display: 'inline-block',
+                            width: 'calc(50% - 8px)',
+                            margin: '0 8px',
+                        }}
+                    >
+                        <InputNumber placeholder='Tuổi' />
+                    </Form.Item>
+                </Form.Item>
+                <Form.Item >
+                    <Input placeholder='Số điện thoại' />
+                </Form.Item>
+                <Form.Item label={null}>
+                    <Button type="primary" htmlType="submit">
+                        Lưu thông tin
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
 
-            <button type="submit" className="text-white w-full h-10 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm">Cập nhật</button>
-        </form>
+
     )
 }
 
